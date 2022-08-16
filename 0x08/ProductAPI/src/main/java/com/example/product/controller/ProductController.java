96331a0191b91dd71a0c39591d301e9a1a7a2adb
Maikon Jakson <107194793+MjaksonCiandT@users.noreply.github.com>
2022-08-15 23:32:58 -0300

    Update ProductController.java

--- 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
@@ -30,7 +30,7 @@ public class ProductController {
     }
 
     @GetMapping(path = "/allProducts")
-    @ApiOperation(value="Retorna uma lista de produtos.")
+    @ApiOperation(value="Responsável por retornar uma lista de produtos.")
     @ApiResponse(code = 11, message = "Warning - the process returned more than 1000 products.")
     public ResponseEntity<List<Product>> getAllProducts(){
         try{
@@ -53,14 +53,14 @@ public class ProductController {
     }
 
     @DeleteMapping("/removeProduct")
-    @ApiOperation(value = "remove um produto.")
+    @ApiOperation(value = "Responsável por remover um produto.")
     @ApiResponses(value = {@ApiResponse(code = 13, message = "User not allowed to remove a product from this category.")})
     public void removeProduct(@RequestBody Product product) {
         productRepository.removeProduct(product);
     }
 
     @PutMapping(path = "/updateProduct")
-    @ApiOperation(value="Atualizar um produto.")
+    @ApiOperation(value="Responsável por atualizar um produto.")
     @ApiResponse(code = 14, message = "No information has been updated. The new information is the same as recorded in the database.")
     public ResponseEntity<Product> updateProduct(@RequestBody Product prod){
         Optional<Product> prodList = Optional.ofNullable((productRepository.getProductById(prod.getId())));
@@ -73,7 +73,7 @@ public class ProductController {
     }
 
     @GetMapping("/welcome")
-    @ApiOperation(value="Retorna uma mensagem de boas vindas.")
+    @ApiOperation(value="Responsável por retornar uma mensagem de boas vindas.")
     public String msgBoasVindas(){
         return "BEM VINDO À PRODUCT REST API.";
     }
